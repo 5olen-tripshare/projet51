8415542caab8df3ed92848f8dfb421db50743dc1
Jovan <jovansubasic@gmail.com>
2025-01-07 18:07:53 +0100

    adding shearch bar and style for card accommodation

--- src/components/accommodationCard.tsx
+++ src/components/accommodationCard.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Star } from "lucide-react";
 
 export function AccommodationCard(props: {
   accommodation: {
@@ -27,7 +26,7 @@ export function AccommodationCard(props: {
 
   return (
     <Link href={`/accommodation/${accommodation.id}`}>
-      <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-transform hover:scale-105 cursor-pointer w-80">
+      <div className="bg-brown border-collapse border border-neutral-950 rounded-lg shadow-xl overflow-hidden transition-transform hover:scale-105 cursor-pointer w-full h-96">
         <div className="relative h-48 w-full">
           <Image
             src={accommodation.image[0]}
@@ -37,21 +36,25 @@ export function AccommodationCard(props: {
           />
         </div>
 
-        <div className="p-4">
-          <p className="text-gray-600 text-sm mb-1">{accommodation.location}</p>
-          <h3 className="text-lg font-semibold text-gray-800">
-            {accommodation.name} - {accommodation.totalPlaces} personnes
-          </h3>
+        <div className="relative p-4 h-48">
+          <div className="max-h-36 overflow-hidden">
+            <p className="text-lightGreen text-sm mb-1">
+              {accommodation.location}
+            </p>
+            <h3 className="text-lg font-semibold text-white ">
+              {accommodation.name} - {accommodation.totalPlaces} personnes
+            </h3>
 
-          <div className="text-gray-500 text-sm mt-2">
-            {accommodation.description}
+            <div className="text-lightGreen text-sm mt-2">
+              {accommodation.description}
+            </div>
           </div>
 
-          <div className="mt-4">
-            <span className="text-lg font-bold text-gray-900">
+          <div className="absolute  bottom-2">
+            <span className="text-lg font-bold text-white">
               {accommodation.price}€
             </span>
-            <span className="text-gray-600"> / nuit</span>
+            <span className="text-lightGreen"> / nuit</span>
           </div>
         </div>
       </div>

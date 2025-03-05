360164a53a17b88157dfdddda0b792e4117fb7a6
NizarNizarNi <120170511+NizarNizarNi@users.noreply.github.com>
2025-03-05 01:30:36 +0100

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,4 +1,39 @@
-variable "namespace" {
-  description = "Namespace pour PostgreSQL HA"
-  default     = "postgres-ha"
+variable "gcp_credentials" {
+  description = "Fichier JSON des identifiants Google Cloud"
+  type        = string
+}
+
+variable "gcp_project" {
+  description = "Nom du projet GCP"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "Région GCP"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "gcp_zone" {
+  description = "Zone GCP"
+  type        = string
+  default     = "europe-west1-b"
+}
+
+variable "os_image" {
+  description = "Image OS pour les VMs"
+  type        = string
+  default     = "rocky-linux-8"
+}
+
+variable "ssh_user" {
+  description = "Nom d'utilisateur pour SSH"
+  type        = string
+  default     = "dakinizar46"
+}
+
+variable "ssh_public_key" {
+  description = "Chemin de la clé publique SSH"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
 }

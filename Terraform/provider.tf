3601821da906e0f76ddd3305946a2f1f9259b272
NizarNizarNi <120170511+NizarNizarNi@users.noreply.github.com>
2025-03-05 01:31:13 +0100

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,3 +1,9 @@
-provider "kubernetes" {
-  config_path = pathexpand("~/.kube/config")
+terraform {
+  required_version = ">= 1.3.0"
+}
+
+provider "google" {
+  credentials = file(var.gcp_credentials)
+  project     = var.gcp_project
+  region      = var.gcp_region
 }

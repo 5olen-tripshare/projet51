cc9ee001d4441a76aaa447ef105d73596d87171e
NizarNizarNi <120170511+NizarNizarNi@users.noreply.github.com>
2025-03-05 10:55:38 +0100

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.gcp_credentials)
+  credentials = var.gcp_credentials 
   project     = var.gcp_project
   region      = "europe-west1"
 }

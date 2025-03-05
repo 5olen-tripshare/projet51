f424d4e34f79bb769b55cb3280030e48fcdf766b
NizarNizarNi <120170511+NizarNizarNi@users.noreply.github.com>
2025-03-05 01:44:20 +0100

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -2,8 +2,4 @@ terraform {
   required_version = ">= 1.3.0"
 }
 
-provider "google" {
-  credentials = file(var.gcp_credentials)
-  project     = var.gcp_project
-  region      = var.gcp_region
-}
+
